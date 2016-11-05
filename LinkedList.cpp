4bf468355514fa7fdeed932513160a6bb0954aca
Andrés Navarro <Andrs Navarro>
2016-11-05 19:34:41 -0300

    funciones listas, falta hacer un main que te de opciones de que queres hacer

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -16,9 +16,16 @@ void add_node( node *ptr, int value  ) // anadir nodo
     new_node -> next = (ptr -> next); //ptr->next es la direccion del nodo futuro, se reemplaza el nodo al que apunta el primero para que el nodo creado apunte a este
     ptr -> next = new_node; // ahora el nodo anterior apunta al nodo creado
 }
-void recorrer_lista(node *ptr, int value)
+void recorrer_lista(node *ptr) //asi recorremos las listas
 {
-	
+	int contador;
+	contador=1; //para saber en que puntero vamos
+	while(ptr!=NULL) //mientras el puntero no sea null es que hay mas punteros
+	{
+		cout<<"Puntero "<<contador<<"="<<ptr->value<<endl; //se imprime la "posicion del puntero" y su valor
+		ptr=ptr->next ; //actualizamos la direccion del puntero por la del siguiente para seguir avanzando en la lista
+		contador=(contador+1);
+	}	
 }
 
 int main()
@@ -27,8 +34,10 @@ int main()
     ptr = new node; //se genera la estructura nodo
     ptr -> value = 1; //valor del primer nodo, es con flechita por el puntero
     ptr -> next = NULL; //el siguiente nodo apunta a null ya que no hay nodo
-    add_node(ptr, 2); //se añade un nodo 
+    add_node(ptr, 2); //se añade un nodo
+	add_node(ptr->next,3);  //otro mas
     cout << ptr <<"-"<< ptr->next <<"-"<<ptr->next->next<<endl;
-    cout << ptr->value << ptr->next->value<<endl; //se imprimen los valores de ambos nodos
+    cout << ptr->value << ptr->next->value<<ptr->next->next->value<<endl; //se imprimen los valores de ambos nodos
+    recorrer_lista(ptr);
     return 0;
 }
